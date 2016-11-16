7f21142880759ed2b23bbdd180b0f940c737f871
Todd Suomela <tsuomela@gmail.com>
2016-11-16 08:55:47 -0500

    initial commit for tracking custom zsh changes, includes path.zsh file

---
+++ path.zsh
@@ -0,0 +1,10 @@
+# Adding user bin directory to the path
+
+path+=/Users/tes023/bin
+
+# Pretty-print of some PATH variables:
+alias path='echo -e ${PATH//:/\\n}'
+alias libpath='echo -e ${LD_LIBRARY_PATH//:/\\n}'
+
+# Alias for opening files from the command line in Emacs
+alias emacs=/Users/tes023/Applications/Emacs.app/Contents/MacOS/Emacs
